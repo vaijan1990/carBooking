417a17bd17e7db10b7b7528894afd57c3112cbcc
vaijan1990 <vaijan1990@gmail.com>
2017-05-19 14:44:22 +0200

    added mongo db connection

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-'use strict';
-var express = require('express');
-var app = express();
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const app = express();
+const url = "mongodb://janani:CarRentalService9!@ds137191.mlab.com:37191/carrentalservice";
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+MongoClient.connect(url, (error, database) => {
+  if(error) console.log(error);
+  console.log("success");
+
+    database.collection('rentalCars').find({}).toArray((error, result) => {
+      console.log(result);
+    });
+});
 
 app.get('/', function(req, res){
   res.send('Hello world');
 });
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log('Example app listening on port 3000!');
 });
