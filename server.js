13c67f10dd8b568888e734433eaab8f0bc28e14b
vaisshnavi janani <vaisshnavi.janani@gmail.com>
2017-05-22 09:33:15 +0200

    added mongo connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('mongodb');  
 const bodyParser = require('body-parser');
 const app = express();
 const url = "mongodb://janani:CarRentalService9!@ds137191.mlab.com:37191/carrentalservice";
@@ -9,19 +10,31 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+let db;
+
 MongoClient.connect(url, (error, database) => {
   if(error) console.log(error);
   console.log("success");
+  
+  db = database;
+  
+  app.listen(port, function(){
+  console.log('App listening on port 3000!');
+});  
+  
+});
 
-    database.collection('rentalCars').find({}).toArray((error, result) => {
-      console.log(result);
+app.get('/rentalCars', (req, res) => {
+  db.collection('rentalCars').find({}).toArray((error, results) => {
+    res.json(results);
+  });
+});
+
+app.get('/rentalCars/:id', (req,res) => {
+  db.collection('rentalCars').findOne(  { 
+      //Have to create a ObjectId from the string in :id
+      "_id" : new mongodb.ObjectId(req.params.id) 
+    }, (error, result)=> {
+      res.json(result);
     });
-});
-
-app.get('/', function(req, res){
-  res.send('Hello world');
-});
-
-app.listen(port, function(){
-  console.log('Example app listening on port 3000!');
 });
