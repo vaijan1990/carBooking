5b43567261933f520e6d540dbdcbd514b3972d91
hkarn <hakan@arnoldson.net>
2017-05-26 16:49:37 +0200

    datepicker front-end working

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,7 @@
 
+$(function () {
+  $.fn.datepicker.defaults.format = "D dd of MM yyyy";
+  $.fn.datepicker.defaults.weekStart = 1;
+  $.fn.datepicker.defaults.orientation = 'bottom';
+  $('.datepicker').datepicker();
+});
