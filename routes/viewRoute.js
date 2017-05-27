7966714c4de119d9b8a9f1e3c926471a1b0715e6
Aknotofknots <christianahlsen@gmail.com>
2017-05-27 19:31:59 +0200

    deleted uneccessary file

--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -17,15 +17,16 @@ router.get('/', (request, response) => {
     response.render('index', {title: 'Hey'})
 });
 
-// the login form is sent to this route
+// the login form request is sent to this route
 router.get('/login', (request, response) => {
     // check if the customer is in the database and the render appropriate view depending on the result.
     // render the index view again, but with the login button changed to logged in. or smth else..
     response.render('index')
 });
 
+// when a customer wants to become a member they're sent here
 router.get('/register', (request, response) => {
-    // render the register form here
+    // render the register form to the client here
     response.send("render a register form here")
 });
 
