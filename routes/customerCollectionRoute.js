0f8644304e0875b34158d8d2879db13bb9e1ff39
vaijan1990 <vaijan1990@gmail.com>
2017-05-27 11:47:16 +0200

    schema for customers

---
+++ routes/customerCollectionRoute.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+
+/*** Routes exports ***/
+//you can simulate post,delete and patch requests with the "postman" software. https://www.getpostman.com/
+
+
+    router.use(function (req,res,next) {
+        console.log("/" + req.method);
+        next();
+    });
+    router.get('/customers', (request, response) => {
+
+        customerDetail.find({},(err, customers) => {
+            if(err)
+                console.log(err);
+            response.send(customers);
+        })
+    });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,10 +9,13 @@ app.set('view engine', 'pug');
 
 /*** Models Imports ***/
 var rentalCar = require('./models/rentalCar');
+var customerDetail = require('./models/customerDetail');
 
 /*** Routes import ***/
 var carRoutes = require('./routes/carCollectionRoute')(rentalCar);
+// var customerRoutes = require('./routes/customerCollectionRoute')(customerDetail);
 var viewRoutes = require('./routes/viewRoute');
+
 
 /*** Miscellaneous ***/
 const url = "mongodb://christianahlsen:carRentalService9!@ds137191.mlab.com:37191/carrentalservice";
@@ -27,6 +30,7 @@ app.use('/public', express.static(__dirname + '/public'));
 
 //this is set so that every route in carCollectionRoute.js uses through localhost:3000/cars
 app.use('/cars', carRoutes);
+
 
 //route views/pages for main folder /
 app.use('/', viewRoutes);
