4de9d6074b8042d891cf6e7eb517cba1356c547d
hkarn <hakan@arnoldson.net>
2017-05-30 10:16:44 +0200

    datepicker sumbit

--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-
-$(function () {
-  $.fn.datepicker.defaults.format = "D dd of MM yyyy";
-  $.fn.datepicker.defaults.weekStart = 1;
-  $.fn.datepicker.defaults.orientation = 'bottom';
-  $('.datepicker').datepicker();
-});
