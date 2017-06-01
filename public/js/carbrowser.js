f47be7a1e8311cef35d8a6806188c1de295a9bac
hkarn <hakan@arnoldson.net>
2017-06-01 23:29:08 +0200

    filter on booleans complete

--- public/js/carbrowser.js
+++ public/js/carbrowser.js
@@ -1,23 +1,20 @@
 $(function () {
 
   var myFilters = {
-    auto: false,
-    towbar: false,
+    auto: true,
+    towbar: true,
     seats2: false,
     seats5: false,
     seats7: false
   };
-  console.log(myFilters);
   $.getJSON("/cars", function(result){
 
 
   })
   .done(function(result) {
-    console.log(result);
     //checks if any filter is set
     var filterSet = false;
     $.each(myFilters, function(key, value) {
-      console.log(value)
       if (value)
         filterSet = true;
     })
@@ -26,9 +23,24 @@ $(function () {
         $( "#car-table" ).append( "<tr><td>" + car.brand + "</td><td>"  + car.seats + "</td><td>" + car.auto + "</td><td>" + car.towbar + "</td><td>" + car.priceperday + "</td></tr>");
       })
     } else {
+      $.each(myFilters, function(key, value) {
+        console.log(key);
+        console.log(value);
+
+      if (value) {
+        result = result.filter(function(attr) {
+          console.log("MAP start: ");
+          console.log(attr[key]);
+            if (attr[key] == true) {
+              console.log("RETURN");
+              return attr;
+            }
+          })
+        }
+      })
+      console.log(result);
       result.forEach(function(car) {
-        if (car.auto == myFilters.auto && car.towbar == myFilters.towbar)
-          $( "#car-table" ).append( "<tr><td>" + car.brand + "</td><td>"  + car.seats + "</td><td>" + car.auto + "</td><td>" + car.towbar + "</td><td>" + car.priceperday + "</td></tr>");
+        $( "#car-table" ).append( "<tr><td>" + car.brand + "</td><td>"  + car.seats + "</td><td>" + car.auto + "</td><td>" + car.towbar + "</td><td>" + car.priceperday + "</td></tr>");
       })
     }
 
